c027f65e453af0b50838780e73ceef5a8c183dd2
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2022-10-10 15:48:25 -0400

    iterate through lists with jinja

--- server.py
+++ server.py
@@ -18,6 +18,17 @@ def greeting(name, num):
 def salutations(name, num):
     return f"Salutations {name}! I could say it {num} more times or do {name * num}"
 
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
 
 
 if __name__=="__main__":
